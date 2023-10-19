0c412d93ba6a443a4e253b8badd62e9a66bcf933
CodeLearners23 <148178959+CodeLearners23@users.noreply.github.com>
2023-10-19 11:21:20 +0530

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -9,7 +9,7 @@ def ayurbot():
     page_bg = """
     <style>
     [data-testid="stAppViewContainer"] {
-    background-image: url("https://c4.wallpaperflare.com/wallpaper/586/603/742/minimalism-4k-for-mac-desktop-wallpaper-preview.jpg");
+    background-image: ;
     background-size: cover;
     }        
     </style>
