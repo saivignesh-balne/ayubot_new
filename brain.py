da48c3adc1e1abac5958b0c2d1c27feaa10ea177
CodeLearners23 <148178959+CodeLearners23@users.noreply.github.com>
2023-11-04 11:42:22 +0530

    Update brain.py

--- brain.py
+++ brain.py
@@ -1,7 +1,7 @@
 c_hi = ("ahoy", "namaste", "aloha", "ciao", "bonjour", "hola", "konnichiwa", "hey", "hi there", "what's up?", "yo", "sup", "hey there", "hiya", "howdy", "hello", "hi", "good morning", "good afternoon", "good evening", "how do you do?", "greetings", "salutations")
 c_bye = ("goodbye", "farewell", "have a nice day", "take care", "until we meet again", "bye", "see you", "see you later", "catch you later", "later", "take it easy", "peace out", "adios", "ciao")
 c_no=("no","nope","nah","never")
-c_yes = ("yup","done", "yes", "yeah", "sure", "ok", "okay", "roger", "you bet", "right", "definitely", "of course", "totally", "i agree", "absolutely", "sure thing", "no problem", "cool", "for sure", "you got it", "no doubt", "all right", "certainly", "affirmative", "indeed", "absolutely", "i concur", "most assuredly", "without a doubt", "that is correct")
+c_yes = ("yup","done", "yeah", "sure", "ok", "okay", "roger", "you bet", "right", "definitely", "of course", "totally", "i agree", "absolutely", "sure thing", "no problem", "cool", "for sure", "you got it", "no doubt", "all right", "certainly", "affirmative", "indeed", "absolutely", "i concur", "most assuredly", "without a doubt", "that is correct")
 
 prakriti = "Prakriti is a term used in traditional Indian philosophy, particularly in Ayurveda, to describe the inherent constitution or nature of an individual. In Ayurveda, it is believed that every person has a unique combination of three fundamental energies or doshas, known as Vata, Pitta, and Kapha, which make up their prakriti."
 bot_intro = "Hi there! I am Chatbot specific to determine the Prakriti of an individual. So now lets determine your Prakriti!"
